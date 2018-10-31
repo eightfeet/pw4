0ddb28385b30202d76c169dbf43f2bf1d2b7c1bd
eightfeet <bajiao000000@163.com>
2018-10-31 16:54:24 +0800

    update

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,4 @@
 const defaultState = {
-	sourceList: [],
-	selected: [],
-	currentdata: [],
-	page: 1,
-	time: 2,
-	initDate: false
 };
 const reducer = (state, action) => {
 	switch (action.type) {
